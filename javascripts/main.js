2c2ae5691763551c74bc3b7a60b4c2c5387445ad
dax <occulture.berlin@gmail.com>
2018-07-12 22:18:30 +0200

    Add basic navigation with call for abstracts, tickets and contact

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
-// no javascripts yet
+$(document).ready(function() {
+  $(".tab-button").on("click", function(){
+    var selected = $(this).attr('id').replace('-button', '');
+
+    // clear 'selected' and designate the currently selected tab-button
+    $("#nav>div.selected").removeClass("selected");
+    $(this).addClass("selected");
+
+    // hide all tabs and show the selected tab
+    $(".tab").hide();
+    $("#"+selected).show();
+  });
+});
