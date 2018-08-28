18c31bc5e23d5e889fbcfe119637a1e7b04c2ed1
dax <occulture.berlin@gmail.com>
2018-08-28 19:06:31 +0200

    Add an emphemeral first-view ticket button

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
   $(".tab-button").on("click", function(){
     var selected = $(this).attr('id').replace('-button', '');
 
+    // hide the immediately visible 'buy tickets' button
+    $("#main-ticket-button").hide();
+
     // clear 'selected' and designate the currently selected tab-button
     $("#nav>li.selected").removeClass("selected");
     $(this).addClass("selected");
