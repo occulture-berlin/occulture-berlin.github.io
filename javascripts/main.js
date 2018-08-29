afbbbde1e0c38a348a79b70603c77eab53082bba
dax <occulture.berlin@gmail.com>
2018-08-29 18:45:11 +0200

    Style updates: new fonts, ticket pointer images and sub-navigation

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,16 +2,29 @@ $(document).ready(function() {
   $(".tab-button").on("click", function(){
     var selected = $(this).attr('id').replace('-button', '');
 
-    // hide the immediately visible 'buy tickets' button
+    // remove initially visible elements
     $("#main-ticket-button").hide();
+    $("#high-priority-information-wrap").hide();
 
     // clear 'selected' and designate the currently selected tab-button
     $("#nav>li.selected").removeClass("selected");
+    $(".sub-nav>li.selected").removeClass("selected");
     $(this).addClass("selected");
 
     // hide all tabs and show the selected tab
     $(".tab").hide();
     $("#"+selected).show();
+
+    // show sub-navigation if relevant
+    if($("#"+selected).hasClass("sub-navigable")) {
+      $("#"+selected+"-sub-nav").show();
+    } else if($("#"+selected).hasClass("sub-tab")) {
+      // re-select the parent nav button to clarify sub-nav origin
+      var parentNavButton = this.parentNode.id.replace('sub-nav', 'button');
+      $("#"+parentNavButton).addClass("selected");
+    } else {
+      $(".sub-nav").hide();
+    };
   });
 });
 
