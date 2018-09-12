efc56c93f26954434de80c0bc00e46fd060dc41f
dax <occulture.berlin@gmail.com>
2018-09-12 22:53:12 +0200

    Add a window resize console monitor for debugging

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+  // log the screen width on resize
+  window.onresize = function(event) {
+    console.log("window width: "+$(window).width()+"px");
+  };
+
   $(".tab-button").on("click", function(){
     var selected = $(this).attr('id').replace('-button', '');
 
