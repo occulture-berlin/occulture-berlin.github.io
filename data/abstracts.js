19d76027542c3c94a4347da89cbba8ef653ad747
dax <occulture.berlin@gmail.com>
2018-09-17 19:00:01 +0200

    Make a speakers-2018 page and allow linking via anchor tags

--- data/abstracts.js
+++ data/abstracts.js
@@ -1,24 +1,28 @@
 abstracts = [
   {
     "name": "Reverend Danny Nemu",
+    "searchString": "reverend-danny-nemu",
     "title": "Keta-chaos, Aya-animism & the Demon of the Ulcer",
     "avatar-path": "images/speakers/nemu-danny-red.jpg",
     "abstract": "<p><center>Myth and plot, metre and rhyme,</br>Structuring life in space and time,</br>Lines of meaning cutting through it,</br>Ink on the page of the master poet.</center></p><p> In dreams, visions and compulsions, invisible intelligences communicate in the language of poetry, and when we call on our gods and familiars, we are answered in rhyme.</p><p>Describing my experiences of treating a flesh-eating parasite with ayahuasca, and the uninvited intervention of one of the demons of the Goetia into my life, this talk is about using Magick and Psychedelick power tools in occult dimensions to sculpt the goo that coalesces into daily experience. We’ll explore some of the best and worst practices for combining the techniques effectively, and how formal structures influence the narrative of our lives, including geometry in space, metre in time, and myth, metaphor and plotline.</p>"
   },
   {
     "name": "John Cussans",
+    "searchString": "john-cussans",
     "title": "Bwa Kayiman and the Simbi-Makaya Egregor",
     "avatar-path": "images/speakers/cussans-john-red.jpg",
     "abstract": "<p>The signal event of the Haitian Revolution was a Vodou ceremony that took place in Bwa Kayiman (Alligator Woods) in August 1791. According to popular legend the Houngan Dutty Boukman and Mambo Cecile Fatiman oversaw a rite in which a Kreyol pig was sacrificed to the Black God of the African slaves and to the Spirit of Liberty. Scholarly speculation about the specific nature of the rite is divided between those who believe it to have been a Dahomian blood-oath, binding the cult to its emancipatory mission, and those who see it as a Petro rite summoning the fierce and violent loa (Vodou spirits) of insurrection.<p></p>A more esoteric theory proposes that, through a special Petro-Zandor rite, an egregor of malevolent loa was created and that a character called Jean-Babtiste Vixamar Legrand offered himself up for sacrifice during the ceremony. Haiti’s historic misfortunes since the revolution, advocates of this theory claim, are a consequence of the egregor’s continued existence. Only after an unbinding rite will this conglomeration of dark-matter entities cease wreaking havoc in the country. For this presentation I will discuss the Simbi-Makaya thesis and the occult notion of the egregor, relating them to traditional accounts of the Bwa Kayiman ceremony and to the traditions of the Bizango secret societies of Haiti.</p>"
   },
   {
     "name": "Dr. Dirk Schlottmann",
+    "searchString": "dr-dirk-schlottmann",
     "title": "Korean Shamanism in the New Millennium",
     "avatar-path": "images/speakers/schlottmann-dirk-red.png",
     "abstract": "<p>In this visual anthropological lecture, Dr. Dirk Schlottmann presents Korean shamanism in the new millennium. The lecture is on one hand designed as a short introduction to different ethnological theories and will help to understand the confusing tangle of definitions that goes along with the terms shamanism, musok and neoshamanism.  On the other hand, the collection of photographs in combination with the explanations will allow a rare and intimate view into the exotic, modern world of Korean shamans and transport a visual aesthetic of religion that is difficult to approach by words alone.<p>"
   },
   {
     "name": "Helene Arts",
+    "searchString": "helene-arts",
     "title": "The Left-Hand Path",
     "avatar-path": "images/speakers/arts-helene-red.jpg",
     "abstract": "<p>This presentation is a high-level overview of the Left-Hand Path practice of Magick.  The distinguishing feature of the Left-Hand Path is its assertion and reliance on Self as the primary source of power – this is in contrast to the belief in a power greater, or other, than Self (ie God/Goddess, the 'Universe', etc.), which is known as the Right-Hand Path. Note that the two paths are not divisive, but do represent the philosophical underpinnings of a given magickal practice. The presentation will be structured as follows: </p> <p> 1. Clear explanation of what the Left-Hand Path is, and isn’t, with examples and references.  </br> 2. Brief overview of some primary symbolic Left-Hand Path figures (ie Lilith, Lucifer, Satan etc) </br> 3. Philosophy and practice of the Left-Hand Path.  </br> 4. Commentary on past and modern contributors to the legacy left for 21st century Left-Hand Path magicians.  </br> 5. Research notes, bibliography and references for follow-up reading and study will be provided to participants.</p>"
@@ -31,36 +35,42 @@ abstracts = [
   },
   {
     "name": "Jamie Gilmartin",
+    "searchString": "jamie-gilmartin",
     "title": "Sacred Geometry, Pythagoras & Technology",
     "avatar-path": "images/speakers/gilmartin-jamie-red.png",
     "abstract": "<p>This talk will tell the tale of Pythagoras illuminating the mythical and controversial aspects of his life ultimately to paint him as the great aggregator of not only Egyptian, Babylonian, and ancient Hebrew mathematics but their mystery schools as well. I will explain the Pythagorean Tetractys using the Hypotenuse app</p><p>I’ve recently released Hypotenuse, a Sacred Geometry App for iOS where one can draw Sacred Geometry in Augmented Reality, over images, as well as learn some basic polygon constructions - it’s great for Sigils!</p>"
   },
   {
     "name": "Robert Dickins",
+    "searchString": "robert-dickins",
     "title": "Reconfiguring the World: Automatic Writing and Authority in the 19 th Century",
     "avatar-path": "images/speakers/dickins-rob-red.png",
     "abstract": "<p>This talk will initially trace the pre-history of ‘automatic writing,’ arguing that it originated in the work of visionaries such as the theologian Emanuel Swedenborg, the prophet Joanna Southcott, and the poet William Blake, and contextualise its function in various domestic writing practices associated with family worship. It will explain how these elements had a direct influence on the popularisation of automatic writing in the 1850s, a development that coincided with the advent of Modern Spiritualism.  It will then trace the various conceptualizations of the writing technique by spiritualists and its critics over the latter half of the nineteenth century. This will provide a visceral snapshot into not only the tensions that arose between secular and spiritual discourse during the period, but also shows how these jarring modes of meaning-making merged to become the complex set of beliefs now understood as automatic writing.</p><p> At the core of this talk is an argument centred on spirit communication as a method for disrupting existing models of authority. This includes the way in which it redefines our relationship with the sacred, particularly away from religious models centred on church-going and into understanding the home and nature landscapes as spheres of spiritual authority. Furthermore, it disrupts notions of personal identity, and gives rise to changing social perceptions of gender roles and the limits of self- understanding. In doing so, it illuminates an important contradiction that permeates occulture generally; by disavowing authorship and authority in undertaking magical practice, the practitioner is simultaneously taking charge of reconfiguring the world, and subverting their place within it.</p>"
   },
   {
     "name": "Jimmy Trash",
+    "searchString": "jimmy-trash",
     "title": "Roots in the Heart (Film Screening)",
     "avatar-path": "images/speakers/alexander-jimmy-red.jpg",
     "abstract": "<p> ROOTS IN THE HEART follows musician Jimmy Trash on a journey through parts of Southern and Eastern Europe that have managed to slip through time and retain their faith and practice of folk methods of healing, seeing and blessing.  </p> <p> Inspired by the words of the 16th century physician Paracelsus, “one must seek out old wives, gypsies, sorcerers, wandering tribes, old robbers, and such outlaws and take lessons from them“, Jimmy Trash travels through six countries actively participating in rituals that have been passed down in oral tradition for centuries. He meets and learns from Granny Whisperers, Roma, shamans and more, himself being the one to be “worked“ on and experiencing the actions.  </p> <p> Along the way he talks with younger people who have a connection with these traditions, in an attempt to obtain a modern view of the ancient techniques. Amongst them are descendants of these traditions who have the burden of keeping their grandparents‘ traditions alive, students of the older healers and ethnographers who study magic in an academic way.  </p> <p> Also featured heavily through the movie is the music and songs of the same communities dealing with everyday magic, as whenever you look for magic, music is never far away.  </p>"
   },
   {
     "name": "Viviana Druga",
+    "searchString": "viviana-druga",
     "title": "Tarot Has Nothing to Do With the Future",
     "avatar-path": "images/speakers/druga-viviana-red.jpg",
     "abstract": "<p> Viviana Druga is using Tarot de Berlin (Major Arcana) to unlock shadow problematics within the individual. It is not about the future but about the present with a connection to the subconscious realm. The workshop will be based on live readings with volunteers. Audience watching is welcomed to interact and ask questions. Come with a question about yourself, something you want more clearance on. You will receive guidance.</p>"
   },
   {
     "name": "Lori Haberkorn",
+    "searchString": "lori-haberkorn",
     "title": "Empowering your Elemental Femininity",
     "avatar-path": "images/speakers/haberkorn-lori-red.png",
     "abstract": "<p>The Golden Circle Rituals are born out of my own spiritual journey of seeking for a deeper meaning in life and like minded women. This Ritual is to empower and support women and men on their own journey to their purest self, to reconnect with mother nature, to learn, grow and flow with her.  There are many different kind of Circles, like the Back to Nature Circle to take us back to what really matters and to our true mission in life, the Empowering Self Love Circle to arise the magik of self love and inner beauty, the Sisterhood Gathering to reconnect with our sisters and femininity and the Moon Circles, as we are all affected and led by the intense power of mother moon. </p><p>I also use these magikal circles to create sacred spaces to reawaken the elemental power, the inner wise human, that already lives deep down in ourselves. As it is finally time to listen to our own intuition and to live in our true power. It is time to rise and shine, to grow and heal together, to go back to yourself, to go back to nature.</p>"
   },
   {
     "name": "Verónica Mota",
+    "searchString": "veronica-mota",
     "title": "Mexico, The Land of the Dead",
     "avatar-path": "images/speakers/mota-veronica-red.png",
     "abstract":"<p>'Mexico, The Land of the Dead' is a research on the effects of the Occult, Santeria, Witchcraft and the cult to the Santa Muerte in Mexico today. We know that this part of the world located in North America, -yes, please check again the map- and on the border to the United States has been terribly affected by the War on Drugs, Cartels, US-Imperialism, internal corruption, and a very dark wave of dead. Thousands of human beings are found decapitated, chopped up, tortured and killed on a daily basis. The most affected are young women and girls. The brutality behind many of this killings makes us think of a very dark cult spreading around the country in addition to military and police brutality. This is a battle of forces and we need to be aware who is in charge of the most violent crimes. This year the so called left won elections in Mexico which opens new posibilities for social action.</p><p>This presentation gives an overview of drugs, violence, political power and the occult behind the scenes. Our goal is to bring awareness and connect with other people how understand the power of rituals and ancestral knowledge. We need to fight back!</p>"
--- javascripts/main.js
+++ javascripts/main.js
@@ -33,19 +33,16 @@ $(document).ready(function() {
   });
 });
 
-// load all speakers by default, or show a given number
+// load all speakers by default, show a given number, or load by name
 loadSpeakers = function(numberOfSpeakers = abstracts.length) {
   $.get("./speakers.html", function(template) {
-    // the 'abstracts' variable is set in data/abstracts.js
-    var shuffled = abstracts.sort(() => .5 - Math.random());
-    var selected = shuffled.slice(0,numberOfSpeakers)
-    var speakerData = selected.map(function(abstract){
+    var speakers = collectSpeakers(numberOfSpeakers);
+
+    var speakerData = speakers.map(function(abstract){
       return Mustache.to_html(template, abstract)
     })
 
-    $("#lineup-wrap #lineup").html(speakerData).append(
-      "<p>...and many more to come</p>"
-    );
+    $("#lineup-wrap #lineup").html(speakerData);
   }, "html");
 }
 
@@ -53,4 +50,41 @@ goToPage = function(path){
   // update urls when clicking a link
   // github pages use a frameset which is fucked, that's the short version.
   top.window.location.href = path
+}
+
+//// INTERNAL FUNCTIONS
+
+// NB: the 'abstracts' variable is set in data/abstracts.js
+collectSpeakers = function(numberOfSpeakers){
+  // get the speaker name from the anchor tag
+  var anchorTag = window.location.hash.substring(1);
+
+  // if no speaker name is given,
+  // return the requested number of speakers in random order
+  if(anchorTag === "") {
+    var shuffled = abstracts.sort(() => .5 - Math.random());
+    return shuffled.slice(0,numberOfSpeakers)
+  };
+
+  // NOTE: important to change object.name to lowercase so the strings match
+  var selected = abstracts.find(object => object.searchString === anchorTag);
+
+  if(selected != null) {
+    return [selected]; // return as a collection
+  } else {
+    // return null object as collection
+    var speakerName = anchorTag.split('-').join(' ');
+    return [{
+      "name": "",
+      "title": "Speaker not found!",
+      "avatar-path": "",
+      "abstract": "<p>Sorry, we don't know anyone called "+speakerName+"!</p>"
+    }]
+  }
+}
+
+getSpeakerNameFromHash = function(){
+  var anchorTag = window.location.hash.substring(1)
+  if(anchorTag === "") { return null };
+
 }
