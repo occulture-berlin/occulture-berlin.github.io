9f95ff6ceec1185c71b7ce6156861f396cb035d5
dax <occulture.berlin@gmail.com>
2018-10-12 11:16:38 +0200

    Remove black moon landing page

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,9 +4,6 @@ $(document).ready(function() {
     console.log("window width: "+$(window).width()+"px");
   };
 
-  // TEMPORARY! Display the landing page for black moon discount
-  displayBlackMoonLandingPage();
-
   $(".tab-button").on("click", function(){
     var selected = $(this).attr('id').replace('-button', '');
 
@@ -53,27 +50,6 @@ goToPage = function(path){
   top.window.location.href = path
   // send this as an event to google analytics
   gtag('event', 'page_view', {'page_path': path});
-}
-
-displayBlackMoonLandingPage = function() {
-  $("#black-moon-landing-wrap").slideDown(1200, function() {
-    $("footer").hide();
-    // select all steps
-    $(".step").each(function(index) {
-      $(this).delay(600*index).fadeIn();
-    });
-  });
-}
-
-hideBlackMoonLandingPage = function() {
-  $("#black-moon-landing-wrap .close-button").fadeOut(300);
-
-  // fade out the components more quickly for a slower, smooter transition
-  $("#black-moon-landing").fadeOut(1200);
-  $("#black-moon-landing-wrap").fadeOut(3000, function() {
-    $("footer").show();
-    displayMailingList();
-  });
 }
 
 // display the mailing list sign-up form
