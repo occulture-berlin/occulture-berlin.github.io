31b2735ba67d795c8a1251a07123e15c815c91cf
dax <d.dax@email.com>
2019-08-25 10:33:00 +0200

    Update event tracking

--- javascripts/main.js
+++ javascripts/main.js
@@ -59,7 +59,12 @@ $(document).ready(function() {
   // track clicks of any element with ga-track class
   $('.ga-track').on('click', function() {
     var name = this.action || $(this).text() || this.value
-    gtag('send', 'event', 'button', 'click-'+name, 1);
+
+    gtag('event', 'click', {
+      'event_category': 'engagement',
+      'event_label': 'click-'+name,
+      //'value': <value>
+    });
   });
 });
 
