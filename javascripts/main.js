60f0078b95cbda60859e79b64edc89d7f3354442
dax <d.dax@email.com>
2019-09-01 01:28:53 +0200

    Don't hide get tickets button after nav swtich :facepalm:

--- javascripts/main.js
+++ javascripts/main.js
@@ -18,6 +18,9 @@ $(document).ready(function() {
     $("#high-priority-information-wrap").addClass('hidden');
     $("#image-carousel").addClass('hidden');
 
+    // switch nav after initial click
+    $('#nav-wrap #nav').children('li').removeClass('hidden');
+
     // clear 'selected' and designate the currently selected tab-button
     $("#nav>li.selected").removeClass("selected");
     $(".sub-nav>li.selected").removeClass("selected");
