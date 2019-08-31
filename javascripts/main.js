1d5cee4a9f5506ccacbaf7d803e93846c711e3c7
dax <d.dax@email.com>
2019-08-31 10:32:43 +0200

    Move image-carousel logic to its own file

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,22 +4,6 @@ $(document).ready(function() {
     console.log("window width: "+$(window).width()+"px");
   };
 
-  // TODO: this should go into it's own file
-  $('#image-carousel #images > li').toggle();
-  $('#image-carousel #images > li:last').toggle().addClass("last-img").
-    prev().toggle().addClass('active-img').
-    prev().toggle().addClass('first-img')
-
-  setInterval(function() {
-    $('#image-carousel #images > li:last').removeClass("last-img").toggle();
-    $('#image-carousel #images').prepend($('#image-carousel #images > li:last'))
-
-    $('#image-carousel #images > li:last').
-      removeClass('active-img').addClass("last-img").
-      prev().removeClass('first-img').addClass('active-img').
-      prev().toggle().addClass('first-img')
-  },  3000);
-
   // landing page
   displayLandingPage();
 
