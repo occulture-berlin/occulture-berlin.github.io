d6a052b015433fd7aa96ccd4e4a7502760b91928
dax <d.dax@email.com>
2019-10-02 21:11:44 +0200

    Update abstract parsing to allow diviners as well

--- lib/generate_event_pages.rb
+++ lib/generate_event_pages.rb
@@ -2,7 +2,7 @@ require 'yaml'
 require 'fileutils'
 
 class GenerateEventPages
-  TEMPLATE_PATH = 'templates/event_page.haml'
+  EVENT_TEMPLATE_PATH = 'templates/event_page.haml'
 
   def self.call(input, target_dir)
     new(input, target_dir).call
@@ -11,7 +11,7 @@ class GenerateEventPages
   def initialize(input, target_dir)
     @events = YAML.load(File.read(input))
     @target_dir = target_dir
-    @template = File.read(TEMPLATE_PATH)
+    @template = File.read(EVENT_TEMPLATE_PATH)
   end
 
   def call
@@ -19,9 +19,11 @@ class GenerateEventPages
     FileUtils.rm_rf(target_dir)
     FileUtils.mkdir(target_dir)
 
-    # build a template page for each confirmed speaker
+    # build a page for each confirmed event in the lineup
     events.each do |event|
-      build_page(event)
+      filename = "#{target_dir}/#{event['searchString']}.haml"
+
+      File.open(filename, 'w') { |f| f.write(build_page(event)) }
     end
 
     nil
@@ -31,17 +33,22 @@ class GenerateEventPages
   attr_reader :events, :target_dir, :template
 
   def build_page(event)
-    filename = "#{target_dir}/#{event['searchString']}.haml"
+    page = template.
+      gsub('__name', event['name']).
+      gsub('__searchString', event['searchString']).
+      gsub('__avatarPath', event['avatarPath'])
 
-    File.open(filename, 'w') do |f|
-      event_page = template.
-        gsub('__name', event['name']).
-        gsub('__searchString', event['searchString']).
-        gsub('__title', event['title']).
-        gsub('__avatarPath', event['avatarPath'])
-
-      f.write(event_page)
+    if event.key?('title')
+      page.gsub!('__title', event['title'])
     end
+
+    if event['type'] == 'Divination'
+      page.gsub!('__pageType', 'divination_page')
+    else
+      page.gsub!('__pageType', 'event_page')
+    end
+
+    page
   end
 end
 
@@ -50,7 +57,7 @@ input = "./_data/events-#{year}.yml"
 target_dir = "./#{year}/lineup"
 
 print "Calling this generator is destructive!\n\n"
-print "If you continue, you will overwrite speaker pages for #{year}\n\n"
+print "If you continue, you will overwrite event pages for #{year}\n\n"
 print "Are you sure you want to proceed? (y/n)"
 
 response = gets.chomp.strip == 'y' ? true : false
