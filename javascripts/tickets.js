10f06b16a40d4987165685126e4f7cbfd83e1d59
dax <d.dax@email.com>
2019-10-06 12:08:44 +0200

    Deploy Sun 06 Oct 2019 12:08:44 PM CEST

---
+++ javascripts/tickets.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  countdown.DEFAULTS = countdown.WEEKS
+
+  // set countdown
+  displayCountdown();
+
+  $('#ticket-info-section-wrap .ticket .name').prepend('+ ');
+
+  $('#ticket-info-section-wrap .ticket .name').on('click', function(){
+    var details = $(this).parent().find('.details');
+
+    details.toggleClass('hidden');
+    if ( details.hasClass('hidden') ) {
+      $(this).text( $(this).text().replace('-', '+') );
+    } else {
+      $(this).text( $(this).text().replace('+', '-') );
+    };
+  });
+});
+
+displayCountdown = function() {
+  var time = countdown(null, new Date(2019, 9, 31), countdown.DEFAULTS).toString();
+      result = "Just "+time+" left!"
+
+  $('#countdown-wrap #countdown').html(result);
+}
