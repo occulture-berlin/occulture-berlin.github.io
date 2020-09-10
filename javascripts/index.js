4a6f47dea522b4016d9b842bb1149148329502e2
dax <d.dax@email.com>
2020-09-10 22:13:18 +0200

    Show upcoming events and adjust landing image

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,19 +1,20 @@
 $(document).ready(function() {
 
-  $('section#past-events .nav li').on('click', function(){
+  $('section#past-events .nav li, section#upcoming-events .nav li').on('click', function(){
     var selected = $(this).text();
+    var events = $(this).closest('section');
 
     //// this is nothing
     $(this).parent().children().removeClass('selected');
 
     // hide all events (clean slate)
-    $('section#past-events .past-event-wrapper').addClass('hidden');
+    events.find('.event-wrapper').addClass('hidden')
 
     //// this is something
     $(this).addClass('selected');
 
     // show information about selected event
-    $('section#past-events').find('#information-'+ selected).removeClass('hidden');
+    events.find('.information-'+ selected).removeClass('hidden');
   });
 
 });
